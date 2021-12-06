baa5c8c8e806c85a96e93e4dcfd7f28983b76459
Andrew Zhubrin <azhubrin+1@codio.com>
2021-12-06 10:38:52 +0000

    Finish django configurations

--- blango/urls.py
+++ blango/urls.py
@@ -23,6 +23,3 @@ urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="post-detail")
 ]
-
-from django.conf import settings
-print(f"Time zone: {settings.TIME_ZONE}")
