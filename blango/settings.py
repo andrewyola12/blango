65317013660172e5f09ddff5aefb2e381cb828cf
Andrew Zhubrin <azhubrin+1@codio.com>
2021-12-08 11:51:58 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -68,6 +68,7 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'blango_auth',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
@@ -166,6 +167,6 @@ class Dev(Configuration):
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
-    
+    AUTH_USER_MODEL = "blango_auth.User"
 class Prod(Dev):
     DEBUG = False
