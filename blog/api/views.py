05bda175d5b53455d741a8d9c8198ec8273b4d3a
Andrew Zhubrin <azhubrin+1@codio.com>
2022-01-18 08:40:25 +0000

    Finish caching

--- blog/api/views.py
+++ blog/api/views.py
@@ -5,14 +5,28 @@ from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 from blango_auth.models import User
 from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer, TagSerializer
 from blog.models import Post, Tag
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
+from rest_framework.exceptions import PermissionDenied
 
     
 class UserDetail(generics.RetrieveAPIView):
+    @method_decorator(cache_page(300))
+    def get(self, *args, **kwargs):
+        return super(UserDetail, self).get(*args, *kwargs)
     lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
 class TagViewSet(viewsets.ModelViewSet):
+    @method_decorator(cache_page(300))
+    def list(self, *args, **kwargs):
+        return super(TagViewSet, self).list(*args, **kwargs)
+
+    @method_decorator(cache_page(300))
+    def retrieve(self, *args, **kwargs):
+        return super(TagViewSet, self).retrieve(*args, **kwargs)
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     @action(methods=["get"], detail=True, name="Posts with the Tag")
@@ -24,6 +38,19 @@ class TagViewSet(viewsets.ModelViewSet):
         return Response(post_serializer.data)
     
 class PostViewSet(viewsets.ModelViewSet):
+    @method_decorator(cache_page(120))
+    def list(self, *args, **kwargs):
+        return super(PostViewSet, self).list(*args, **kwargs)
+    @method_decorator(vary_on_headers("Authorization"))
+    @method_decorator(vary_on_cookie)
+    @action(methods=["get"], detail=False, name="Posts by the logged in user")
+    def mine(self, request):
+        if request.user.is_anonymous:
+            raise PermissionDenied("You must be logged in to see which Posts are yours")
+        posts = self.get_queryset().filter(author=request.user)
+        serializer = PostSerializer(posts, many=True, context={"request": request})
+        return Response(serializer.data)
+
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
 
